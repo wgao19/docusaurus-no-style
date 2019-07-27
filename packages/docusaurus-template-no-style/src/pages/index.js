11d5bee1f9b5b93a9f2caca04a270f970c4d4f42
Wei Gao <wgao19@gmail.com>
2019-07-27 04:04:05 +0000

    clean up no style template home page
    - remove original content
    - swizzle footer
    - put some intro content in home page

--- packages/docusaurus-template-no-style/src/pages/index.js
+++ packages/docusaurus-template-no-style/src/pages/index.js
@@ -7,43 +7,7 @@
 
 import React from 'react';
 import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import withBaseUrl from '@docusaurus/withBaseUrl';
-import styles from './styles.module.css';
-
-const features = [
-  {
-    title: <>Easy to Use</>,
-    imageUrl: 'img/undraw_docusaurus_mountain.svg',
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
-  },
-  {
-    title: <>Focus on What Matters</>,
-    imageUrl: 'img/undraw_docusaurus_tree.svg',
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: <>Powered by React</>,
-    imageUrl: 'img/undraw_docusaurus_react.svg',
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
-  },
-];
 
 function Home() {
   const context = useDocusaurusContext();
@@ -52,31 +16,14 @@ function Home() {
     <Layout
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
-      <header className={styles.heroBanner}>
-        <h1>{siteConfig.title}</h1>
-        <p>{siteConfig.tagline}</p>
-        <Link className={styles.getStarted} to={withBaseUrl('docs/doc1')}>
-          Get Started
-        </Link>
-      </header>
       <main>
-        {features && features.length && (
-          <section className={styles.features}>
-            {features.map(({imageUrl, title, description}, idx) => (
-              <div key={idx} className={styles.feature}>
-                {imageUrl && (
-                  <img
-                    className={styles.featureImage}
-                    src={withBaseUrl(imageUrl)}
-                    alt={title}
-                  />
-                )}
-                <h3>{title}</h3>
-                <p>{description}</p>
-              </div>
-            ))}
-          </section>
-        )}
+        <h1>content of home page</h1>
+        <ul>
+          <li>
+            footer is a swizzled component with minimum styling using CSS
+            Modules
+          </li>
+        </ul>
       </main>
     </Layout>
   );
