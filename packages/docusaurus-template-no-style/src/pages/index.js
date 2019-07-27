ae72c8a2d63a8f5144591d11ac8eddde3f1283f7
Wei Gao <wgao19@gmail.com>
2019-07-27 16:43:00 +0000

    copy docs to home page

--- packages/docusaurus-template-no-style/src/pages/index.js
+++ packages/docusaurus-template-no-style/src/pages/index.js
@@ -6,6 +6,7 @@
  */
 
 import React from 'react';
+import Link from '@docusaurus/Link';
 import Layout from '@theme/Layout';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 
@@ -17,13 +18,66 @@ function Home() {
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <main>
-        <h1>content of home page</h1>
-        <ul>
-          <li>
-            footer is a swizzled component with minimum styling using CSS
-            Modules
-          </li>
-        </ul>
+        <article>
+          <h1>Docusaurus Theme No Style</h1>
+          <p>
+            This theme is a trim down from{' '}
+            <a href="https://github.com/facebook/docusaurus/tree/master/packages/docusaurus-theme-classic">
+              @docusaurus/theme-classic
+            </a>{' '}
+            of <a href="http://docusaurus-2.netlify.com/">Docusaurus 2</a> to
+            contain only <em>opinionated minimum styles</em>:
+          </p>
+          <ul>
+            <li>
+              all css selectors are using only semantic html tags with roles, no
+              class names used
+            </li>
+            <li>
+              docs and blog page layout with grid with 2 side sections and 1
+              main in middle
+            </li>
+            <li>accessory sections have paddings with a fixed value 1rem</li>
+          </ul>
+          <h2 id="overwriting-styles-for-this-theme">
+            Overwriting styles for this theme
+          </h2>
+          <p>
+            Depending on how fine-grained you wish to overwrite the styles, you
+            have a few options:
+          </p>
+          <p>
+            To modify the styling without changing the DOM structure of
+            individual components, you may swizzle the <code>Layout</code>{' '}
+            component and update the CSS there:
+          </p>
+          <pre>
+            <code class="language-shell">
+              $ yarn swizzle @docusaurus/theme-no-style Layout
+            </code>
+          </pre>
+          <p>
+            This will copy the <code>Layout</code> component to your site's{' '}
+            <code>src</code> directory. Restart your dev server, and then the
+            site will be using the component in your code.
+          </p>
+          <p>
+            Most of the styles provided in this theme is inside the{' '}
+            <code>Layout</code> component.
+          </p>
+          <p>
+            If you need to modify the DOM structure and / or styling for
+            individual component, swizzle the corresponding component with the
+            same command.
+          </p>
+          <p>
+            To see the list of components provided by this theme, refer{' '}
+            <a href="#">here</a>.
+          </p>
+          <h3>
+            <Link to="/docs/doc1">Now start reading</Link>
+          </h3>
+        </article>
       </main>
     </Layout>
   );
