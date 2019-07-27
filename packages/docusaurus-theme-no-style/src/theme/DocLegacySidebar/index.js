3d8ce67448e7b03657d1874cf29a1bfa5bcf8930
Wei Gao <wgao19@gmail.com>
2019-07-27 04:17:39 +0000

    clean sidebar for no style theme
    - use aside html tag
    - use css and remove css module
    - remove responsivity

--- packages/docusaurus-theme-no-style/src/theme/DocLegacySidebar/index.js
+++ packages/docusaurus-theme-no-style/src/theme/DocLegacySidebar/index.js
@@ -6,16 +6,10 @@
  */
 
 import React, {useState} from 'react';
-import classnames from 'classnames';
-
 import Link from '@docusaurus/Link';
-
-import styles from './styles.module.css';
-
-const MOBILE_TOGGLE_SIZE = 24;
+import './styles.css';
 
 function DocLegacySidebar(props) {
-  const [showResponsiveSidebar, setShowResponsiveSidebar] = useState(false);
   const {docsMetadata, sidebar} = props;
 
   if (!sidebar) {
@@ -49,26 +43,15 @@ function DocLegacySidebar(props) {
     switch (item.type) {
       case 'category':
         return (
-          <li className="menu__list-item" key={item.label}>
-            <a className="menu__link" href="#!">
-              {item.label}
-            </a>
-            <ul className="menu__list">{item.items.map(renderItem)}</ul>
+          <li key={item.label}>
+            <a href="#!">{item.label}</a>
+            <ul>{item.items.map(renderItem)}</ul>
           </li>
         );
-
       case 'link':
         return (
-          <li className="menu__list-item" key={item.label}>
-            <Link
-              activeClassName="menu__link--active"
-              className="menu__link"
-              to={item.href}
-              onClick={() => {
-                setShowResponsiveSidebar(false);
-              }}>
-              {item.label}
-            </Link>
+          <li key={item.label}>
+            <Link to={item.href}>{item.label}</Link>
           </li>
         );
 
@@ -79,51 +62,7 @@ function DocLegacySidebar(props) {
   };
 
   return (
-    <div className={styles.sidebar}>
-      <div
-        className={classnames('menu', 'menu--responsive', {
-          'menu--show': showResponsiveSidebar,
-        })}>
-        <button
-          aria-label={showResponsiveSidebar ? 'Close Menu' : 'Open Menu'}
-          className="button button--secondary button--sm menu__button"
-          type="button"
-          onClick={() => {
-            setShowResponsiveSidebar(!showResponsiveSidebar);
-          }}>
-          {showResponsiveSidebar ? (
-            <span
-              className={classnames(
-                styles.sidebarMenuIcon,
-                styles.sidebarMenuCloseIcon,
-              )}>
-              &times;
-            </span>
-          ) : (
-            <svg
-              className={styles.sidebarMenuIcon}
-              xmlns="http://www.w3.org/2000/svg"
-              height={MOBILE_TOGGLE_SIZE}
-              width={MOBILE_TOGGLE_SIZE}
-              viewBox="0 0 32 32"
-              role="img"
-              focusable="false">
-              <title>Menu</title>
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeMiterlimit="10"
-                strokeWidth="2"
-                d="M4 7h22M4 15h22M4 23h22"
-              />
-            </svg>
-          )}
-        </button>
-        <ul className="menu__list">
-          {thisSidebar.map(item => renderItem(item, {root: true}))}
-        </ul>
-      </div>
-    </div>
+    <aside>{thisSidebar.map(item => renderItem(item, {root: true}))}</aside>
   );
 }
 
