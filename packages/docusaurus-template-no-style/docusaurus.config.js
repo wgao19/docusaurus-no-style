fe953f09f1d201a6f1f4900c1169f8a3af3825e0
Wei Gao <wgao19@gmail.com>
2019-07-27 16:49:59 +0000

    cleanup docusaurus.config.js

--- packages/docusaurus-template-no-style/docusaurus.config.js
+++ packages/docusaurus-template-no-style/docusaurus.config.js
@@ -6,97 +6,77 @@
  */
 
 module.exports = {
-  title: "My Site",
-  tagline: "The tagline of my site",
-  url: "https://your-docusaurus-test-site.com",
-  baseUrl: "/",
-  favicon: "img/favicon.ico",
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  title: 'Docusaurus no style',
+  tagline: 'Docusaurus 2 theme with minimum style',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  favicon: 'img/favicon.ico',
+  organizationName: 'wgao19', // Usually your GitHub org/user name.
+  projectName: 'docusaurus-no-style', // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: "My Site",
+      title: 'My Site',
       logo: {
-        alt: "My Site Logo",
-        src: "img/logo.svg"
+        alt: 'My Site Logo',
+        src: 'img/logo.svg',
       },
       links: [
-        { to: "docs/doc1", label: "Docs", position: "left" },
-        { to: "blog", label: "Blog", position: "left" },
+        {to: 'docs/doc1', label: 'Docs', position: 'left'},
+        {to: 'blog', label: 'Blog', position: 'left'},
         {
-          href: "https://github.com/facebook/docusaurus",
-          label: "GitHub",
-          position: "right"
-        }
-      ]
+          href: 'https://github.com/facebook/docusaurus',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
     },
     footer: {
-      style: "dark",
+      style: 'dark',
       links: [
         {
-          title: "Docs",
+          title: 'Docs',
           items: [
             {
-              label: "Docs",
-              to: "docs/doc1"
-            }
-          ]
+              label: 'Docs',
+              to: 'docs/doc1',
+            },
+          ],
         },
         {
-          title: "Community",
+          title: 'Community',
           items: [
             {
-              label: "Discord",
-              href: "https://discordapp.com/invite/docusaurus"
-            }
-          ]
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/docusaurus',
+            },
+          ],
         },
         {
-          title: "Social",
+          title: 'Social',
           items: [
             {
-              label: "Blog",
-              to: "blog"
-            }
-          ]
-        }
+              label: 'Blog',
+              to: 'blog',
+            },
+          ],
+        },
       ],
       logo: {
-        alt: "Facebook Open Source Logo",
-        src: "https://docusaurus.io/img/oss_logo.png"
+        alt: 'Facebook Open Source Logo',
+        src: 'https://docusaurus.io/img/oss_logo.png',
       },
-      copyright: `Copyright © ${new Date().getFullYear()} Facebook, Inc. Built with Docusaurus.`
-    }
+      copyright: `Copyright © ${new Date().getFullYear()} Facebook, Inc. Built with Docusaurus.`,
+    },
   },
-  themes: [
-    "@docusaurus/theme-no-style"
-    // Don't add this if algolia config is not defined
-    // algolia && "@docusaurus/theme-search-algolia"
-  ],
+  themes: ['@docusaurus/theme-no-style'],
   plugins: [
     [
-      "@docusaurus/plugin-content-docs-legacy",
+      '@docusaurus/plugin-content-docs-legacy',
       {
-        sidebarPath: require.resolve("./sidebars.js")
-      }
+        sidebarPath: require.resolve('./sidebars.js'),
+      },
     ],
-    ["@docusaurus/plugin-content-blog"],
-    ["@docusaurus/plugin-content-pages"]
-    // isProd && googleAnalytics && "@docusaurus/plugin-google-analytics",
-    // isProd && gtag && "@docusaurus/plugin-google-gtag",
-    // isProd && ["@docusaurus/plugin-sitemap"]
-  ]
-  // presets: [
-  //   [
-  //     require.resolve("../preset"),
-  //     {
-  //       docs: {
-  //         sidebarPath: require.resolve("./sidebars.js")
-  //       },
-  //       theme: {
-  //         customCss: require.resolve("./src/css/custom.css")
-  //       }
-  //     }
-  //   ]
-  // ]
+    ['@docusaurus/plugin-content-blog'],
+    ['@docusaurus/plugin-content-pages'],
+  ],
 };
